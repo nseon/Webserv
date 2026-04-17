57bff0f637ca33b0fda9b2791437dd848c07d842
lcesbron <190340561+lcesbron@users.noreply.github.com>
2026-04-17 17:05:26 +0200

    fix: fixed the todo fixme job

--- CI/checkTodoFixMe.sh
+++ CI/checkTodoFixMe.sh
@@ -2,6 +2,6 @@
 
 # original credit goes to kporceil
 
-if [[ $(cat $(find . \( -name "*.c" -o -name "*.h" \) -type f) | grep -c "TODO:") -ne 0 || $(cat $(find . \( -name "*.c" -o -name "*.h" \) -type f) | grep -c "FIXME:") -ne 0 ]]; then
+if [[ $(cat $(find . \( -name "*.cpp" -o -name "*.hpp" \) -type f) | grep -c "TODO") -ne 0 || $(cat $(find . \( -name "*.cpp" -o -name "*.hppp" \) -type f) | grep -c "FIXME") -ne 0 ]]; then
 	false
 fi
