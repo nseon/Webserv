63faabbdc6ad20a2581f1f6c42b2b88c4c585388
lcesbron <190340561+lcesbron@users.noreply.github.com>
2026-05-07 17:34:44 +0200

    fet: WIP cgi

--- src/cgi/CGIHandler.cpp
+++ src/cgi/CGIHandler.cpp
@@ -4,6 +4,12 @@
 CGIHandler::CGIHandler(void) {}
 CGIHandler::~CGIHandler(void) {}
 
+bool	isCgi(std::string URI,
+		std::map<std::string, std::string>	cgi_configs)
+{
+	
+}
+
 void	CGIHandler::setEnvs(std::string URI, std::string )
 {
 	
