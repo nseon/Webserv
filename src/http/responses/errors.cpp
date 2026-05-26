87d18f43296ad8913c2f9a5d45ac8ab6bd761263
nseon <nseon@student.42lyon.fr>
2026-05-26 16:29:52 +0200

    add: error() is finish

--- src/http/responses/errors.cpp
+++ src/http/responses/errors.cpp
@@ -6,14 +6,16 @@
 /*   By: nseon <nseon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/11 15:40:21 by nseon             #+#    #+#             */
-/*   Updated: 2026/05/13 14:11:00 by nseon            ###   ########.fr       */
+/*   Updated: 2026/05/26 14:40:45 by nseon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sstream>
 #include <string>
 #include <vector>
+#include <iostream>
 #include "http/Response.hpp"
+#include "http/responses.hpp"
 
 static std::vector<char> generate_default_error_page(int code, std::string msg)
 {
@@ -36,8 +38,21 @@ int error(Response &response, int code, std::string msg)
 	std::string error_path = response.getLocation().getErrorPath(code);
 	
 	if (error_path.empty())
+	{
+		std::stringstream ss;
+		
 		response.setBody(generate_default_error_page(code, msg));
+		ss << response.getBody().size();
+		response.addHeader("Content-Length", ss.str());
+	}
 	else
-	 	;
+		if (getRessource(error_path, response))
+		{
+			std::stringstream ss;
+			
+			response.setBody(generate_default_error_page(code, msg));
+			ss << response.getBody().size();
+			response.addHeader("Content-Length", ss.str());
+		}
 	return (response.getStatusCode());
 }
